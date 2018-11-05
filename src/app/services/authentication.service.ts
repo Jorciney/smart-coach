721dca7e6b97d26e5c5a0adbee0f8be22e2b790b
Jorciney <jorciney.dias.chaveiro@telenetgroup.be>
2018-11-05 14:42:13 +0100

    added authentication service

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
+import * as firebase from 'firebase';
+import {Observable} from 'rxjs';
+import {Router} from '@angular/router';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthenticationService {
+    public firebaseUser: Observable<firebase.User>;
+    private userDetails: firebase.User = null;
+
+    constructor(private firebaseAuth: AngularFireAuth, private router: Router) {
+        this.firebaseUser = this.firebaseAuth.authState;
+    }
+
+    signingWithGoogle(): Promise<any> {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        return this.firebaseAuth.auth.signInWithPopup(provider)
+            .then(fireBaseUser => this.userDetails = fireBaseUser.user);
+    }
+
+    signingWithFb(): Promise<any> {
+        // TODO test and fix signing with FB
+        return this.firebaseAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+    }
+
+    logout() {
+        this.firebaseAuth.auth.signOut()
+            .then(res => this.router.navigate(['/']));
+    }
+}
